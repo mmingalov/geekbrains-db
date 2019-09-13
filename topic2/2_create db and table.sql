b37b5bb392faeac92800cab67dc9cf75a314d4ae
mmingalov <mmingalov@gmail.com>
2019-09-13 11:31:31 +1000

    !

---
+++ topic2/2_create db and table.sql
@@ -0,0 +1,9 @@
+/* #2
+ *  Создайте базу данных example, разместите в ней таблицу users, состоящую из двух столбцов, числового id и строкового name
+ *  Launch via command: source D:/Cloud/Git/geekbrains-db/topic2/2_create db and table.sql;
+ */
+
+drop database if exists example;
+create database example;
+use example;
+create table users (id int primary key, name nvarchar(100) not null);
---
+++ topic2/4_dump-mysql-help_keyword-10.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mysql
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `help_keyword`
+--
+
+DROP TABLE IF EXISTS `help_keyword`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `help_keyword` (
+  `help_keyword_id` int(10) unsigned NOT NULL,
+  `name` char(64) NOT NULL,
+  PRIMARY KEY (`help_keyword_id`),
+  UNIQUE KEY `name` (`name`)
+) /*!50100 TABLESPACE `mysql` */ ENGINE=InnoDB DEFAULT CHARSET=utf8 STATS_PERSISTENT=0 ROW_FORMAT=DYNAMIC COMMENT='help keywords';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `help_keyword`
+--
+-- WHERE:  true limit 10
+
+LOCK TABLES `help_keyword` WRITE;
+/*!40000 ALTER TABLE `help_keyword` DISABLE KEYS */;
+INSERT INTO `help_keyword` VALUES (224,'(JSON'),(225,'->'),(227,'->>'),(46,'<>'),(616,'ACCOUNT'),(413,'ACTION'),(40,'ADD'),(637,'ADMIN'),(107,'AES_DECRYPT'),(108,'AES_ENCRYPT');
+/*!40000 ALTER TABLE `help_keyword` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-09-13 11:22:10
