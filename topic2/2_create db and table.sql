f47f7e3ddca9c1f2d06916a32c40430a5b9d6c55
mmingalov <mmingalov@gmail.com>
2019-09-13 12:03:48 +1000

    !

--- topic2/2_create db and table.sql
+++ topic2/2_create db and table.sql
@@ -1,6 +1,6 @@
 /* #2
  *  Создайте базу данных example, разместите в ней таблицу users, состоящую из двух столбцов, числового id и строкового name
- *  Launch via command: source D:/Cloud/Git/geekbrains-db/topic2/2_create db and table.sql;
+ *  Launch via command: source D:/Cloud/Git/geekbrains-db/topic2/2_create db and table.sql ;
  */
 
 drop database if exists example;
