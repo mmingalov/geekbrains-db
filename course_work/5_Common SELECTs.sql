8ba3998c832a5337e55cc120ed8c31af1e765c5d
mmingalov <mmingalov@gmail.com>
2019-10-30 18:13:51 +0800

    5 updated

--- course_work/5_Common SELECTs.sql
+++ course_work/5_Common SELECTs.sql
@@ -5,3 +5,11 @@ select L.project 'Месторождение', L.SITE_ID '№ Скважины',
 from lithology L inner join lkp_code C on L.LITH = C.CODE and C.CATEGORY = 'lith_code'
 group by L.project, L.SITE_ID, L.LITH, C.DESCRIPTION
 
+--Координаты скважин (актуальные перезамеры) 
+with table1 as (
+	select project, site_id, max(instance) 'instance'
+	from site_survey
+	group by project, site_id
+)
+select * from site_survey s inner join table1 t on s.project = t.project and s.site_id = t.site_id and s.instance = t.instance
+;
